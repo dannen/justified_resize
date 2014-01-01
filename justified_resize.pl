76cbcc25ca7a98bb9a1860e374fc8146de63a507
Dannen Harris <jher23@gmail.com>
2014-01-01 14:35:06 -0600

    added gif, animated gif, and png resizing.

--- justified_resize.pl
+++ justified_resize.pl
@@ -6,8 +6,6 @@
 #
 # dannen harris (c) 2013
 
-umask 033;
-
 # expected sizes as per Justified-Gallery (aka flickr) expectations
 #
 #"_t": thumbnail: 100px  on longest side
@@ -21,6 +19,8 @@ umask 033;
 # array of size options and suffixes
 %sizes = ( 't', '100', 'm', '240', 'n', '320', 'z', '640', 'c', '800', 'b', '1024' );
 
+# target directory for resized images
+$resizeddir = "resized";
 
 # find out where we are
 $pwd = `pwd`;
@@ -36,35 +36,76 @@ close DIR;
 
 # resize each image, excluding the previously resized images
 foreach $picture (sort keys %dirnames ) {
-        if ( ( $picture =~ /.jpg$/ )  && ( $picture !~ /_[b,c,n,m,t,z]/ ) ) {
-		&resize;
-        }
+	chomp $picture;
+        if ( $picture !~ /_[b,c,n,m,t,z]\./ ) {
+        	if ( $picture =~ /\.jpg$/ ) {
+			&resize;
+        	} elsif ( ( $picture =~ /\.gif$/) || ( $picture =~ /\.png/) ) {
+			&testanimated;
+			&resize;
+		}
+	}
 }
 close DIR;
 
 # set perms on directory and files
-system ("chmod 755 .");
-system ("chmod 644 *.jpg");
+#system ("chmod 755 .");
+#system ("chmod 644 *.jpg");
+#system ("chmod 644 *.gif");
+#system ("chmod 644 *.png");
+
+# examine gifs
+sub testanimated {
+
+	# animated png aka apng files cannot be detected as below, need to find an alternative method
+	$animated = 0;
+
+	print "Testing image $picture\n";
+
+	# detect number of frames in image
+	my $frames = `identify -format %n $picture`;
+	chomp $frames;
+	#print "frames = $frames\n";
+
+	if ( $frames >= "2" ) {
+		$animated = 1;
+	}
+	if ( $animated == 1 ) {
+		print "$picture is animated.\n\n";
+	} else {
+		print "$picture is not animated.\n\n";
+	}
+	
+}
 
 # resize images
 sub resize {
 
-	chomp $picture;
         ($image,$suffix) = split (/\./, $picture);
 
 	print "Working with $picture\n";
 	foreach $key (sort keys %sizes) {
 	
-		$target = join "", $image, "_", $key, ".jpg";
+		$target = join "", $image, "_", $key, ".", $suffix;
+		print "target is $target\n";
 
 		# only copy original for resizing if it doesn't already exist
-		if ( ! -e $target ) {
-			system ("cp $picture $target");		
+		if ( ! -e "$resizeddir/$target" ) {
+			
+			# make a copy of original. if scale > original dimensions, original image will be used rather than upscaling
+			mkdir $resizeddir,0777;
+			system ("cp $picture $resizeddir/$target");		
 		
 
 			# identify width and height
-                	my $ratio = `identify -format \"\%wx\%h\" $target`;
-                	chomp $ratio;
+                	my $fullratio = `identify -format \"\%wx\%h:\" $resizeddir/$target`;
+                	chomp $fullratio;
+			#print "fullratio $fullratio\n";
+
+			# animated images have 100x100:100x100: so we take the first value split on : to compensate
+			(my $ratio, $null) = split ( /:/, $fullratio);
+
+			# parse dimensions of image
                 	($width, $height) = split ( /x/, $ratio);
                 	$height = int ($height);
                 	$width  = int ($width);
@@ -73,14 +114,36 @@ sub resize {
 			my $scale = $sizes{$key};
 			#print "key: $key\n scale: $scale\n";
 
-                	if ( $height > $width ) {
-				# optional parsing for wider than tall
-                        	# print "wider than taller $height x $width $target\n";
-                        	system ("mogrify -scale x$scale  $target");
-                	} else {
-				# no special processing yet for taller than wide
-                        	system ("mogrify -scale x$scale  $target");
-                	}
+			if ( ( $height > $scale) || ( $width > $scale) ) {
+				if ( $suffix =~ /jpg/ ) {
+                			if ( $height > $width ) {
+						# optional parsing for wider than tall
+                        			# print "wider than taller $height x $width $target\n";
+                        			system ("mogrify -scale x$scale  $resizeddir/$target");
+                			} else {
+						# no special processing yet for taller than wide
+                        			system ("mogrify -scale x$scale  $resizeddir/$target");
+					}
+                		} elsif ( $suffix =~ /gif/ ) {
+					print "processing $suffix\n";
+					if ( $animated == 1 ) {
+						# resize animated gifs or pngs
+						# darken and sharpen image slighly during resizing
+						print "animated\n";
+						#system ("convert -sharpen 2 -gamma 0.75 -size $original_dimensions $picture -resize $new_dimensions $target");
+						system ("convert -sharpen 2 -gamma 0.75 $picture -scale x$scale $resizeddir/$target");
+					} else {
+						#print "not animated\n";
+						# no special processing yet for taller than wide
+						system ("mogrify -scale x$scale  $resizeddir/$target");
+					}
+                		} elsif ( $suffix =~ /png/ ) {
+					# animated png/apng files not presently supported
+					print "processing $suffix\n";
+					system ("mogrify -scale x$scale  $resizeddir/$target");
+
+				}
+			}
 		}
 	}
 }
